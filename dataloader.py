e884fbdaffd61b413566f36da150794e554f4d25
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-28 11:47:59 +0300

    add labels map for the case that the labels are not in range of the labels num

--- dataloader.py
+++ dataloader.py
@@ -4,15 +4,19 @@ import numpy as np
 import cv2
 from PIL import Image
 import tensorflow as tf
+from augmentationHelper import get_random_augment
+
 import os
 
 SPLIT_FACTOR = "$"
 
 
-def read_image(path, resize_image=()):
+def read_image(path, resize_image=(), augment=False):
     image = Image.open(path, 'r')
     if image.mode != 'RGB':
         image = image.convert('RGB')
+    if augment:
+        image = get_random_augment(image, resize_image)
     if len(resize_image) > 0:
         image = image.resize(resize_image, Image.NEAREST)
     image = np.array(image).astype(np.float32)
@@ -49,19 +53,17 @@ class DataLoader:
         unique_labels = np.unique(self.datasets["train"][1])
         new_labels = np.arange(0, len(unique_labels))
         self.labels_map = dict(zip(unique_labels, new_labels))
-        print(self.labels_map)
 
 
     def read_batch(self, batch_size, mode):
         all_paths, all_labels = self.datasets[mode]
         rand_idx = np.random.randint(low=0, high=len(all_paths)-1, size=batch_size).astype(np.int)
 
-        # batch_labels = all_labels[rand_idx]
         batch_images = np.zeros((batch_size, self.input_size[0], self.input_size[1], 3))
         labels = []
         b_idx = 0
         for i in rand_idx:
-            batch_images[b_idx, :, :, :] = read_image(all_paths[i], self.input_size)
+            batch_images[b_idx, :, :, :] = read_image(all_paths[i], self.input_size, augment=(mode == "train"))
             label = self.labels_map[all_labels[i]]
             self.paths_logger[mode].append(all_paths[i])
             self.labels_logger[mode].append(label)

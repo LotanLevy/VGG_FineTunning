2082bcbbe1cc157fbfcc3795e2572188a5de6ce4
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-28 11:18:52 +0300

    add labels map for the case that the labels are not in range of the labels num

--- dataloader.py
+++ dataloader.py
@@ -43,26 +43,32 @@ class DataLoader:
 
         self.datasets = read_dataset_map(dataset_file)
 
+        unique_labels = np.unique(self.datasets[1])
+        new_labels = np.arange(0, len(unique_labels))
+        self.labels_map = dict(zip(unique_labels, new_labels))
+
 
     def read_batch(self, batch_size):
         all_paths, all_labels = self.datasets
         rand_idx = np.random.randint(low=0, high=len(all_paths)-1, size=batch_size).astype(np.int)
 
-        batch_labels = all_labels[rand_idx]
+        # batch_labels = all_labels[rand_idx]
         batch_images = np.zeros((batch_size, self.input_size[0], self.input_size[1], 3))
+        labels = []
         b_idx = 0
         for i in rand_idx:
             batch_images[b_idx, :, :, :] = read_image(all_paths[i], self.input_size)
+            label = self.labels_map[all_labels[i]]
             self.paths_logger.append(all_paths[i])
-            self.labels_logger.append(all_labels[i])
+            self.labels_logger.append(label)
+            labels.append(label)
             b_idx += 1
-        return batch_images, batch_labels
+        return batch_images, np.array(labels)
 
     def __del__(self):
-        for mode in self.paths_logger:
-            with open(os.path.join(self.output_path, "{}.txt".format(self.name)), 'w') as f:
-                for i in range(len(self.paths_logger)):
-                    f.write("{}{}{}".format(self.paths_logger[i], SPLIT_FACTOR, self.labels_logger[i]))
+        with open(os.path.join(self.output_path, "{}.txt".format(self.name)), 'w') as f:
+            for i in range(len(self.paths_logger)):
+                f.write("{}{}{}".format(self.paths_logger[i], SPLIT_FACTOR, self.labels_logger[i]))
 
 
 

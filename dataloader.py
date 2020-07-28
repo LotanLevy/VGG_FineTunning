8ad942deb17972d6878f3306f68a134c21518b9f
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-28 11:04:41 +0300

    undo dataloader changes

--- dataloader.py
+++ dataloader.py
@@ -4,7 +4,6 @@ import numpy as np
 import cv2
 from PIL import Image
 import tensorflow as tf
-import random
 import os
 from augmentationHelper import get_random_augment
 
@@ -23,11 +22,9 @@ def read_image(path, resize_image=(), augment=False):
     image = np.array(image).astype(np.float32)
     return image
 
-def read_dataset_map(data_map_path, shuffle=False):
+def read_dataset_map(data_map_path):
     with open(data_map_path, "r") as lf:
         lines_list = lf.read().splitlines()
-        if shuffle:
-            random.shuffle(lines_list)
         lines = [line.split(SPLIT_FACTOR) for line in lines_list]
         images, labels = [], []
         if len(lines) > 0:
@@ -35,69 +32,47 @@ def read_dataset_map(data_map_path, shuffle=False):
         labels = [int(label) for label in labels]
     return images, np.array(labels).astype(np.int)
 
+
+
 class DataLoader:
-    def __init__(self, name, dataset_file, cls_num, input_size, output_path, augment=False):
+
+    def __init__(self, train_file, val_file, test_file, cls_num, input_size, name="dataloader",
+                 output_path=os.getcwd()):
         self.classes_num = cls_num
         self.input_size = input_size
-        self.augment = augment
 
         self.name = name
         self.output_path = output_path
-        self.paths_logger = []
-        self.labels_logger = []
-        self.batch_idx = 0
+        self.paths_logger = {"train": [], "val": [], "test": []}
+        self.labels_logger = {"train": [], "val": [], "test": []}
+
+        self.datasets = {"train": read_dataset_map(train_file),
+                       "val": read_dataset_map(val_file),
+                       "test": read_dataset_map(test_file)}
 
 
+    def read_batch(self, batch_size, mode):
+        all_paths, all_labels = self.datasets[mode]
+        rand_idx = np.random.randint(low=0, high=len(all_paths)-1, size=batch_size).astype(np.int)
 
-        self.datasets = read_dataset_map(dataset_file, shuffle=True)
-        unique_labels = np.unique(self.datasets[1])
-
-        # assert len(unique_labels) == cls_num
-        new_labels = np.arange(0, len(unique_labels))
-        self.labels_map = dict(zip(unique_labels, new_labels))
-        print(self.labels_map)
-        self.batches_idx = 0
-        self.epochs = 0
-
-
-
-
-    def read_batch(self, batch_size):
-        all_paths, all_labels = self.datasets
-
-        # takes the next batch, if it finish the epoch it'll start  new epoch
-
-        indices = list(range(self.batches_idx, min(self.batches_idx + batch_size, len(all_paths))))
-        if len(indices) < batch_size: # new epoch
-            self.batches_idx = 0
-            rest = batch_size - len(indices)
-            indices += list(range(self.batches_idx, min(self.batches_idx + rest, len(all_paths))))
-            self.epochs += 1
-        self.batches_idx += batch_size
-
+        batch_labels = all_labels[rand_idx]
         batch_images = np.zeros((batch_size, self.input_size[0], self.input_size[1], 3))
-        paths = []
-        labels = []
         b_idx = 0
-        for i in indices:
-            batch_images[b_idx, :, :, :] = read_image(all_paths[i], self.input_size, augment=self.augment)
-            paths.append(all_paths[i])
-            labels.append(self.labels_map[all_labels[i]])
-            self.paths_logger.append(all_paths[i])
-            self.paths_logger.append(self.labels_map[all_labels[i]])
+        for i in rand_idx:
+            batch_images[b_idx, :, :, :] = read_image(all_paths[i], self.input_size)
+            self.paths_logger[mode].append(all_paths[i])
+            self.labels_logger[mode].append(all_labels[i])
             b_idx += 1
-
-        hot_vecs = tf.keras.utils.to_categorical(np.array(labels), num_classes=self.classes_num)
-        return batch_images, np.array(labels)
-
-    # def read_batch(self, batch_size):
-    #     batch_images, hot_vecs, paths, labels = self.read_batch_with_details(batch_size)
-    #     self.paths_logger += paths
-    #     self.labels_logger += labels
-    #     return batch_images, hot_vecs
+        return batch_images, batch_labels
 
     def __del__(self):
-        with open(os.path.join(self.output_path, "{}.txt".format(self.name)), 'w') as f:
-            for i in range(len(self.paths_logger)):
-                f.write("{}{}{}\n".format(self.paths_logger[i], SPLIT_FACTOR, self.labels_logger[i]))
+        for mode in self.paths_logger:
+            with open(os.path.join(self.output_path, "{}_{}.txt".format(self.name, mode)), 'w') as f:
+                for i in range(len(self.paths_logger[mode])):
+                    f.write("{}{}{}".format(self.paths_logger[mode][i], SPLIT_FACTOR, self.labels_logger[mode][i]))
+
+
+
+
+
 
